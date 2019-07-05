26f3219d3b9ee718e3c1942a9b2e4f875d673a85
Tim Fernandez-Hart <tim.fernandezhart@gmail.com>
2019-07-05 11:12:26 +0100

    BLE works. Not implemented classic yet.

---
+++ main/dsp_ble.h
@@ -0,0 +1,21 @@
+/*
+ * dsp_ble.h
+ *
+ *  Created on: 27 Jun 2019
+ *      Author: timfernandez
+ */
+
+#ifndef MAIN_DSP_BLE_H_
+#define MAIN_DSP_BLE_H_
+
+enum {
+	IDX_SVC,
+	IDX_CHAR_VOL,
+	IDX_CHAR_VOL_VAL,
+
+	NUM_TABLE_ELEMENTS,
+};
+
+
+
+#endif /* MAIN_DSP_BLE_H_ */
